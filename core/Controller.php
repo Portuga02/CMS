804091cd90f361922cde1e62acc20759dd181c31
saviogomesdasilva@yahoo.com <saviogomesdasilva@yahoo.com>
2020-10-10 18:02:19 -0300

    Foi organizado e criado variaveis para  a organização e conexão com o codigo

--- core/Controller.php
+++ core/Controller.php
@@ -1,13 +1,18 @@
 <?php
 class Controller {
+	private $config;
 
+	public function __construct() {
+		$cfg = new Config();
+		$this->config = $cfg->getConfig();
+	}
 	public function loadView($viewName, $viewData = array()) {
 		extract($viewData);
 		require 'views/' . $viewName . '.php';
 	}
 
 	public function loadTemplate($viewName, $viewData = array()) {
-		require 'views/template.php';
+		require 'views/templates/' . $this->config['site_template'] . '.php';
 	}
 
 	public function loadViewInTemplate($viewName, $viewData = array()) {
