572be0755be653f4419b787927ac427c757cb472
saviogomesdasilva@yahoo.com <saviogomesdasilva@yahoo.com>
2020-11-17 20:11:51 -0300

    configurando layout controller

--- core/Controller.php
+++ core/Controller.php
@@ -1,21 +1,26 @@
 <?php
-class Controller {
+class Controller
+{
 	private $config;
 
-	public function __construct() {
+	public function __construct()
+	{
 		$cfg = new Config();
 		$this->config = $cfg->getConfig();
 	}
-	public function loadView($viewName, $viewData = array()) {
+	public function loadView($viewName, $viewData = array())
+	{
 		extract($viewData);
 		require 'views/' . $viewName . '.php';
 	}
 
-	public function loadTemplate($viewName, $viewData = array()) {
+	public function loadTemplate($viewName, $viewData = array())
+	{
 		require 'views/templates/' . $this->config['site_template'] . '.php';
 	}
 
-	public function loadViewInTemplate($viewName, $viewData = array()) {
+	public function loadViewInTemplate($viewName, $viewData = array())
+	{
 		extract($viewData);
 		require 'views/' . $viewName . '.php';
 	}
