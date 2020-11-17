7bc25a3ab42db84bba0e7f4fce2480266a76037c
saviogomesdasilva@yahoo.com <saviogomesdasilva@yahoo.com>
2020-11-17 20:11:08 -0300

    configurando layout do sistema

--- views/templates/default.php
+++ views/templates/default.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title><?php echo $this->config['site_title']; ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE; ?>assets/css/template.css">
 </head>
+
 <body>
 	<div class="topo">
 
 	</div>
 	<div class="menu"> </div>
-	<div class="container"> <?php $this->loadViewInTemplate($viewName, $viewData);?></div>
+	<div class="container"> <?php $this->loadViewInTemplate($viewName, $viewData); ?></div>
 	<div class="rodape"> </div>
 
 
 
 </body>
+
 </html>
